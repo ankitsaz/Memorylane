be2505c1522ef4dd283270884aeb59b88e9d92ea
Ankit Sahu <128829449+ankitsaz@users.noreply.github.com>
2024-08-12 18:52:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors(
+  {
+    origin: [],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 
 app.use('/user', userRoutes);
 app.use('/posts', postRoutes);
